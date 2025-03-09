bc9cde87254ec76550d497b23b0c52c93f3d8223
gautam gavande <gautamgavande2436@gmail.com>
2025-03-09 13:48:35 +0530

    practice math function and if else

--- script.js
+++ script.js
@@ -164,6 +164,41 @@
 
 //math function
 
+// console.log(Math.ceil(12.5)) //ak aage kar deta hai
+
+// console.log(Math.floor(10.7)) // float value ko hata deta hai
+
+// console.log(Math.round(10.5))//11 //round kar deta hai value ko like 10.4 raha to 10 return karenga agr 10.5 ya usse upar denge to 11 return karenga 
+
+// console.log(Math.round(10.4)) //10
+
+// console.log(Math.abs(-52))//52 //positive value return karta hai
+// console.log(Math.abs(52)) //52
+
+// console.log(Math.trunc(12.72436789)) //point ke bad ke number ko hata deta hai
+
+// console.log(Math.pow(10,2)) // 10 ki power 2
+// console.log(Math.random()) //0 se lekar 1 ke bicha ki value return karta hai
+
+// console.log(Math.sqrt(36)) //6 //squre root return karta hai
+
+// console.log(Math.cbrt(216)) // cube root return karta hai
+
+// console.log(Math.min(10,10,2,3,45,47,89)) // sabse chhoti value return karta hai
+
+// console.log(Math.max(10,20,30,45,65,22)) //maximum value return karta hai
+
+// console.log(Math.random()) //random value return karta hai 0 se lekar 1 ke bich me
+
+
+// let a=10.235689
+// console.log(a.toFixed(3))//point ke bad kitne number return karna hai iske lia use karte hai
+
+
+/////////////////////////////////////////////////////////////////
+//math function
+
+/*
 console.log(Math.ceil(12.5)) //ak aage kar deta hai
 
 console.log(Math.floor(10.7)) // float value ko hata deta hai
@@ -191,7 +226,88 @@ console.log(Math.max(10,20,30,45,65,22)) //maximum value return karta hai
 console.log(Math.random()) //random value return karta hai 0 se lekar 1 ke bich me
 
 
+
 let a=10.235689
 console.log(a.toFixed(3))//point ke bad kitne number return karna hai iske lia use karte hai
 
+*/
 
+//1.calculate compund intrest
+
+// cp=a-p;
+// a=p*(1+r/100)^t
+
+// let p= Number(prompt("Enter principle"))
+// let r=Number(prompt("Emter a rate"))
+// let t=Number(prompt("Enter a time"))
+
+// /*
+
+// A=p*(1+r/100)^t
+// CP=A-P
+
+// */
+
+// console.log((p*Math.pow(1+r/100,t)) - p ).toFixed(2)
+
+
+//2. genrate otp
+// console.log(Math.floor(Math.random()*9000+1000))
+
+
+
+//3.heros formula 
+//s=a+b+c/2
+//area of tringle=s*(s-a)*(s-b)*(s-c)^1/2
+
+// let a=Number(prompt("Enter a first nunber"))
+// let b=Number(prompt("Enter a second nunber"))
+// let c=Number(prompt("Enter a third nunber"))
+
+// if(a+b<=c ||a+c<=b||b+c<=a){
+//     console.log("not possible ")
+// }else{
+//     s=(a+b+c)/2
+//     console.log(Math.sqrt(s*(s-a)*(s-b)*(s-c)))
+// }
+
+//4 circumfrence of ciecle
+
+// let r=Number(prompt("Enter a number"));
+// console.log(2*Math.PI*r)
+// console.log(Math.pow(Math.PI*r,2)) //area
+
+
+//5 if else 
+
+// if(10<9|| 10>11){
+//     console.log("hello world")
+// }else if(10<15){
+//     console.log("mai bhi hu")
+// }
+// else{
+//     console.log("mai tu chalunga")
+// }
+
+
+//6.two gretest number
+
+// let a=Number(prompt("enter first number"))
+// let b=Number(prompt("enter second number"))
+
+// if(a>b)console.log("first number is greter")
+// else console.log("second number is greter")
+
+
+//7.even odd Number
+
+// let a=Number(prompt("Enter a number"))
+
+// if(a%2==0) console.log("number is even")
+// else console.log("number is odd")
+
+//8.valid voter or not
+// let age=Number(prompt("Enter your age "))
+// let name=prompt("Enter your name ")
+// if(age>=18)console.log(`${name} is valid voter`)
+// else console.log("you are not valid voter")
